fa2185a46635708ff5bbc944d70b857ebe38a5b6
Varun Kumar <varunkumar1@virtualemployee.com>
2021-12-09 13:51:00 +0530

    convert main layout into functional component

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -1,36 +1,36 @@
-import React, {Component} from 'react';
-import $ from 'jquery'
+import React, {useEffect} from 'react';
 
 import CommonStyle from '../assets/css/style.js';
 import Header from './layouts/Header';
 import Sidebar from './layouts/Sidebar';
 import Footer from './layouts/Footer';
 
-class MainLayout extends Component {
-    componentDidMount(){
-        $('[data-toggle=offcanvas]').click(function() {
-            $('.row-offcanvas').toggleClass('active');
-        });
-    }
+const MainLayout = (props) => {
+    /**hook equivalent to componentdidmount lifecycle */
+    useEffect(() => {
+        const canvas = document.querySelector('[data-toggle=offcanvas]');
+        const row = document.querySelector('.row-offcanvas');
+        canvas.addEventListener('click', function(){
+            row.classList.toggle('active');
+        })
+    }, []);// eslint-disable-line react-hooks/exhaustive-deps
 
-    render() {
-        return(
-            <CommonStyle>
-                <Header {...this.props.children.props}></Header>
-                <div className="container-fluid h-100" id="main">
-                    <div className="row row-offcanvas row-offcanvas-left h-100 active">
-                        <Sidebar {...this.props.children.props} ></Sidebar>
-                        <div className="col main pt-5 mt-3">
-                            {/** main content shows here */}
-                            {this.props.children} 
-                            
-                            <Footer {...this.props.children.props}></Footer>
-                        </div>
+    return(
+        <CommonStyle>
+            <Header {...props.children.props}></Header>
+            <div className="container-fluid h-100" id="main">
+                <div className="row row-offcanvas row-offcanvas-left h-100 active">
+                    <Sidebar {...props.children.props} ></Sidebar>
+                    <div className="col main pt-5 mt-3">
+                        {/** main content shows here */}
+                        {props.children} 
+                        
+                        <Footer {...props.children.props}></Footer>
                     </div>
-                </div>    
-            </CommonStyle>
-        )
-    }
+                </div>
+            </div>    
+        </CommonStyle>
+    )
 }
 
 export default MainLayout;
