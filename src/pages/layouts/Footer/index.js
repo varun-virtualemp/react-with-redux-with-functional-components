f8aae07be8077fd8a94123c8c8eb91b0207963f5
Varun Kumar <varunkumar1@virtualemployee.com>
2021-12-09 14:31:09 +0530

    footer styling

--- src/pages/layouts/Footer/index.js
+++ src/pages/layouts/Footer/index.js
@@ -5,7 +5,7 @@ import FooterStyle from './style';
 const Footer = (props) => {
     return (
         <FooterStyle>
-            <footer className="main-footer">
+            <footer className="main-footer fixed-bottom">
                 <strong>Copyright © 2014-2021</strong>
             </footer>
         </FooterStyle>
--- src/pages/layouts/Footer/style.js
+++ src/pages/layouts/Footer/style.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 
 export default styled.div`
     footer{
-        position: absolute;
-        bottom:0;
-        background-color: #fff;
-        color: #869099;
-        padding: 1rem;
-        width: 82%
+        background-color : #fff;
+        color : #869099;
+        padding : 1rem;
+        bottom : 0;
+        left : auto;
+        width : 83%
     }
 `;
